ebcd9ff6ec14452d6eb47e5098f74753be34ddf3
Swarali <swaralimore3@gmail.com>
2026-04-06 17:01:00 +0530

    Array basics - create, print, indexing, loop

---
+++ Array/create_print_indexing_loop/practice_task.py
@@ -0,0 +1,22 @@
+##Q1 - Create an array of 5 fruits and print the last fruit
+
+fruits = ["Apple","Banana","Mango"]
+print(fruits[-1])
+
+##Q2 - Print only the 3rd element of this array
+
+arr = [10, 20, 30, 40, 50]
+print(arr[2])
+
+##Q3 - Print all elements of this array using a loop
+
+arr = ["apple", "banana", "mango"]
+for i in arr:
+    print(i)
+
+##Print each element with its index
+
+arr = [5, 10, 15, 20]
+for i in range(len(arr)):
+    print(i,arr[i])
+
