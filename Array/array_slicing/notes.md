08317f5e1f781399937e81fcfed5abe12977f0f7
Swarali <swaralimore3@gmail.com>
2026-04-06 22:26:35 +0530

    Add:array-slicing notes wirh practice-tasks

---
+++ Array/array_slicing/notes.md
@@ -0,0 +1,7 @@
+arr = [10, 20, 30, 40, 50]
+
+arr[1:4]   # [20, 30, 40] → index 1 to 3
+arr[:3]    # [10, 20, 30] → start to index 2
+arr[2:]    # [30, 40, 50] → index 2 to end
+arr[:]     # [10, 20, 30, 40, 50] → full array
+arr[-2:]   # [40, 50] → last 2 elements
---
+++ Array/array_slicing/practice_task.py
@@ -0,0 +1,18 @@
+##Q1 - First 3 elements kadhh:
+arr1 = [10, 20, 30, 40, 50]
+print(arr1[0:3])
+
+##Q2 - Last 2 elements kadhh:
+arr2 = ["apple", "banana", "mango", "orange"]
+print(arr2[-2:])
+
+##Q3 - Middle part kadhh (20, 30, 40):
+arr3 = [10, 20, 30, 40, 50]
+print(arr3[1:4])
+
+##Q4 - He sang - output kay asel?:
+arr = [5, 10, 15, 20, 25]
+print(arr[1:4])
+print(arr[:2])
+print(arr[3:])
+
